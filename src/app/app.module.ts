b693862b17a5c5f5085d33dbce871a0198f65711
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-10-08 12:17:59 +0000

    Added routing and OKTA authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+
+const oktaConfig = {
+  issuer: 'https://dev-354685.oktapreview.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oagk0vssioRGOgqI0h7'
+};
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'trivia', component: TriviaGameComponent },
+    { path: 'implicit/callback', component: OktaCallbackComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TriviaGameComponent } from './trivia-game/trivia-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TriviaGameComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      RouterModule.forRoot(routes),
+      OktaAuthModule.initAuth(oktaConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
